9f90855203b9d1a1612cfce759ca0fd5149e3ab4
Aymon Delbridge <aymon.delbridge@gmail.com>
2017-05-06 21:23:14 +0200

    Simplified the generate contained within main.

--- terraingenerator.go
+++ terraingenerator.go
@@ -22,19 +22,16 @@ func main() {
 
 	log.Println("Generating world.")
 	// Generate the 2d slice of sizeY rows, and sizeX columns
+	// Traverse the 2D array and set random numbers
+	// Confused by the X,Y coordinates? Rows are Y. Columns X
 	world := make([][]int32, terrain.SizeY)
 	for y := 0; y < terrain.SizeY; y++ {
 		world[y] = make([]int32, terrain.SizeX)
-	}
-
-	// Traverse the 2D array and set random numbers
-	// Confused by the X,Y coordinates. Rows are Y. Columns X.
-	for y := 0; y < terrain.SizeY; y++ {
 		for x := 0; x < terrain.SizeX; x++ {
-			c := rand.Int31n(100)
-			world[y][x] = c
+			world[y][x] = rand.Int31n(100)
 		}
 	}
+
 	draw(world, terrain)
 }
 
