40e6ce114e3ad99e3497f976ec1b36fc3cb600cc
Aymon Delbridge <aymon.delbridge@gmail.com>
2017-05-07 17:27:11 +0200

    Outputting simple png terrain map.

--- terraingenerator.go
+++ terraingenerator.go
@@ -7,6 +7,8 @@ import (
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/fogleman/gg"
 )
 
 // Terrain is just the world meta? data
@@ -50,11 +52,34 @@ func generate(terrain *Terrain, terrainTypes map[int]string) [][]int {
 func draw(world [][]int, terrain *Terrain) {
 	log.Println("Drawing world.")
 	for y := range world {
-		log.Println(world[y])
-	}
-	for y := range world {
 		fmt.Println(world[y])
 	}
+	dc := gg.NewContext(1600, 1600)
+	dc.SetRGB(0, 0, 0)
+	for y := 0; y < terrain.SizeY; y++ {
+		for x := 0; x < terrain.SizeX; x++ {
+			fmt.Println(world[y][x])
+			dc.Push()
+			switch world[y][x] {
+			case 0:
+				dc.SetRGB(0, 0, 255)
+			case 1:
+				dc.SetRGB(0, 20, 200)
+			case 2:
+				dc.SetRGB(0, 255, 0)
+			case 3:
+				dc.SetRGB(0, 200, 20)
+			case 4:
+				dc.SetRGB(60, 60, 60)
+			}
+			dc.DrawRectangle(float64(x*20), float64(y*20), 20.0, 20.0)
+			dc.Fill()
+			dc.Pop()
+
+		}
+	}
+	dc.SavePNG("out.png")
+
 }
 
 // initialise returns an instance of Terrain
@@ -79,9 +104,4 @@ func initialise() *Terrain {
 		SizeX: sizeX,
 		SizeY: sizeY,
 	}
-}
-
-// Polygon draws a simple polygon. Current a Square
-func Polygon(n int) []Point {
-	return result
 }
